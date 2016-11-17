77ab360cf9f1b9f7b11773077f49eb05c158abfb
Nikol <nricano87@gmail.com>
2016-11-16 21:45:00 -0600

    plantilla

---
+++ app/assets/javascripts/counter.js
@@ -0,0 +1,10 @@
+$(function(){
+ 
+  $("#getting-started")
+    .countdown("2017/01/01", function(event) {
+      $(this).text(
+          event.strftime('%D days %H:%M:%S')
+          );
+    });
+});
+
